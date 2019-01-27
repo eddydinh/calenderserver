7cdec65aaa2d6bc4dc9f01ec872d5cbc5ac25d65
Eddy Dinh <minhdinh25599@gmail.com>
2019-01-26 19:40:33 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const fileupload = require("express-fileupload");
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: 'digmed1!',
+        database: 'calendar'
+    }
+});
+
+const app = express();
+
+
+app.use(bodyParser.json());
+app.use(fileupload());
+app.use(cors());
+app.use(express.static('public'));
+
+app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+
+
+app.listen(3000, ()=> {
+  console.log('app is running on port 3000');
+})
