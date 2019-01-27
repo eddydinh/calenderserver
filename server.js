2337b19868a068bbb608263fd07e942d5ac95456
Eddy Dinh <minhdinh25599@gmail.com>
2019-01-26 23:30:35 -0800

    sdfsd

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.get('/:fileName',function(req,res,next){
 
 app.get('/unparsedfiles', (req,res)=>{
    db('users').returning('*').select('*').then(response =>{
-       res.json(response);
+       res.json("success");
    }).catch(error=>res.status(400).json('Unable to fetch markers information'))
 })
 
