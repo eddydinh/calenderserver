e535128be70def27fa057d7f13a1aa06c0b95766
Eddy Dinh <minhdinh25599@gmail.com>
2019-01-26 20:45:49 -0800

    new end points

--- server.js
+++ server.js
@@ -33,6 +33,52 @@ app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 
+app.get('/:fileName',function(req,res,next){
+    
+  const options = {
+    root: __dirname + '/public/files/',
+    dotfiles: 'deny',
+    headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+    }
+  };
+ const fileName = req.params.fileName;
+    
+    res.sendFile(fileName,options,(err)=>{
+        if(err){
+            next(err);
+        }else{
+            console.log('Sent: ', fileName);
+        }
+    })
+});
+
+app.post('/', (req,res)=>{
+    const {filename} = req.body;
+
+    if(!req.files){
+        
+        res.json("File Not Found!");
+    }else{
+    let uploadFile = req.files.file;
+       uploadFile.mv(`${__dirname}/public/files/${filename}`,  (err) => {
+        if (err) {
+            return res.status(500).send(err)
+        }
+        
+    //DATABASE CODE GOES HERE
+        db('users').insert({
+        
+        filename:filename
+    
+    }).then(console.log).catch(error=>res.status(400).json('Unable to add marker'));
+    res.json('success');
+
+    }) 
+}  
+})
+
 
 app.listen(process.env.PORT || 3000, ()=> {
   console.log(`app is running on port ${process.env.PORT}`);
