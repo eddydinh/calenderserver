5d9a0d1f1052c46b8a948538b2cc845af06fdd2a
Eddy Dinh <minhdinh25599@gmail.com>
2019-01-27 00:01:36 -0800

    sfsdf

--- controllers/unparsed.js
+++ controllers/unparsed.js
@@ -1,5 +1,5 @@
 const handleUnparsedGet = (req, res, db) => {
-  db.select('*').from('users')
+  db.select('*').from('users').where('id',1)
     .then(user => {
       if (user.length) {
         res.json(user[0])
