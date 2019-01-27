c4573f3bde832470e90730a69f71f4a9e41b3d09
Eddy Dinh <minhdinh25599@gmail.com>
2019-01-27 00:15:28 -0800

    sdfds

--- controllers/unparsed.js
+++ controllers/unparsed.js
@@ -1,11 +1,11 @@
 const handleUnparsedGet = (req, res, db) => {
  
-  db.select('*').from('users').where({1})
+  db.select('*').from('users').where('id',1)
     .then(user => {
       if (user.length) {
         res.json(user[0])
       } else {
-        res.status(400).json('Not fucking found')
+        res.status(400).json('Not found')
       }
     })
     .catch(err => res.status(400).json('error getting user'))
