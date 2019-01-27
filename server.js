649d89f953d7b107aede0d09dfdc92aae9cf6609
Eddy Dinh <minhdinh25599@gmail.com>
2019-01-26 19:45:49 -0800

    enviroment variable

--- server.js
+++ server.js
@@ -33,6 +33,6 @@ app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcry
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 
 
-app.listen(3000, ()=> {
-  console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, ()=> {
+  console.log(`app is running on port ${process.env.PORT}`);
 })
