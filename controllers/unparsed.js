b6b2e285885b55264f1278a03514ee687101de52
Eddy Dinh <minhdinh25599@gmail.com>
2019-01-27 00:19:58 -0800

    sdfdsf

--- controllers/unparsed.js
+++ controllers/unparsed.js
@@ -1,6 +1,6 @@
 const handleUnparsedGet = (req, res, db) => {
  
-  db.whereNotNull('filename').andWhereNull('parsedfile').select('*')
+  db.whereNotNull('filename')
     .then(user => {
       if (user.length) {
         res.json(user[0])
